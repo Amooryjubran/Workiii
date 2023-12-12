8cf9d3fe6cebff7dc3d0c156a3a4c3a40289b2b3
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-12-11 20:12:33 -0500

    Dashboard Page | Conditionally rendering tabs based on the user type and role

--- app/src/pages/Dashboard/dashboardTabs.jsx
+++ app/src/pages/Dashboard/dashboardTabs.jsx
@@ -1,14 +1,17 @@
 import Services from "@/components/ui/Dashboard/Services";
 import Users from "@/components/ui/Dashboard/Users";
+import Categories from "@/components/ui/Dashboard/Categories";
 import { useTranslation } from "react-i18next";
 import SettingsImg from "images/Dashboard/settings.svg";
 import UsersImg from "images/Dashboard/users.svg";
 import CategoriesImg from "images/Dashboard/categories.svg";
-import Categories from "@/components/ui/Dashboard/Categories";
+import useUserStore from "@/store/useUserStore";
+
 export const useDashboardTabs = () => {
   const { t } = useTranslation();
+  const { user } = useUserStore();
 
-  const tabs = [
+  const allTabs = [
     {
       label: t("dashboard.Services"),
       component: <Services />,
@@ -19,14 +22,21 @@ export const useDashboardTabs = () => {
       component: <Users />,
       image: UsersImg,
       action: "resetUserTab",
+      roles: ["admin"],
     },
     {
       label: t("dashboard.Categories"),
       component: <Categories />,
       image: CategoriesImg,
+      roles: ["admin"],
     },
-    // ... other tabs would be added here
+    // ... other tabs
   ];
 
-  return tabs;
+  // Adjusted filter logic
+  const filteredTabs = allTabs.filter(
+    (tab) => !tab.roles || (user?.userType && tab.roles.includes(user.userType))
+  );
+
+  return filteredTabs;
 };
